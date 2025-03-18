091b397484016e3842fbe111c60833f3d63920f1
J-Ajsivinac <meso.israelajsivinac@gmail.com>
2025-03-17 20:51:51 -0600

    feat(database): Add new database schema for advanced team statistics and related entities

--- src/database/schema/DB.sql
+++ src/database/schema/DB.sql
@@ -1,13 +1,3 @@
--- Generado por Oracle SQL Developer Data Modeler 24.3.1.351.0831
---   en:        2025-03-13 11:30:48 CST
---   sitio:      Oracle Database 11g
---   tipo:      Oracle Database 11g
-
-
-
--- predefined type, no DDL - MDSYS.SDO_GEOMETRY
-
--- predefined type, no DDL - XMLTYPE
 
 CREATE TABLE Advanced_team_stats 
     ( 
@@ -187,7 +177,7 @@ CREATE TABLE Players
      height               VARCHAR2 (4000)  NOT NULL , 
      weight               VARCHAR2 (4000)  NOT NULL , 
      playercode           VARCHAR2 (4000)  NOT NULL , 
-     greatest_75_flag     BLOB  NOT NULL , 
+     greatest_75_flag     BLOB, 
      Countries_id_country INTEGER  NOT NULL , 
      Teams_id_team        INTEGER  NOT NULL 
     ) 
@@ -527,48 +517,3 @@ ALTER TABLE Teams
      id_state
     ) 
 ;
-
-
-
--- Informe de Resumen de Oracle SQL Developer Data Modeler: 
--- 
--- CREATE TABLE                            17
--- CREATE INDEX                             2
--- ALTER TABLE                             36
--- CREATE VIEW                              0
--- ALTER VIEW                               0
--- CREATE PACKAGE                           0
--- CREATE PACKAGE BODY                      0
--- CREATE PROCEDURE                         0
--- CREATE FUNCTION                          0
--- CREATE TRIGGER                           0
--- ALTER TRIGGER                            0
--- CREATE COLLECTION TYPE                   0
--- CREATE STRUCTURED TYPE                   0
--- CREATE STRUCTURED TYPE BODY              0
--- CREATE CLUSTER                           0
--- CREATE CONTEXT                           0
--- CREATE DATABASE                          0
--- CREATE DIMENSION                         0
--- CREATE DIRECTORY                         0
--- CREATE DISK GROUP                        0
--- CREATE ROLE                              0
--- CREATE ROLLBACK SEGMENT                  0
--- CREATE SEQUENCE                          0
--- CREATE MATERIALIZED VIEW                 0
--- CREATE MATERIALIZED VIEW LOG             0
--- CREATE SYNONYM                           0
--- CREATE TABLESPACE                        0
--- CREATE USER                              0
--- 
--- DROP TABLESPACE                          0
--- DROP DATABASE                            0
--- 
--- REDACTION POLICY                         0
--- 
--- ORDS DROP SCHEMA                         0
--- ORDS ENABLE SCHEMA                       0
--- ORDS ENABLE OBJECT                       0
--- 
--- ERRORS                                   0
--- WARNINGS                                 0
