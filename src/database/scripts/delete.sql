b3a3a67c8ad5fb13e877a0deb69d51435c5b57b7
J-Ajsivinac <meso.israelajsivinac@gmail.com>
2025-04-01 18:23:35 -0600

    chore(database): Remove obsolete sequences and truncate temporary tables in delete script

--- src/database/scripts/delete.sql
+++ src/database/scripts/delete.sql
@@ -4,9 +4,29 @@ DROP SEQUENCE state_seq;
 DROP SEQUENCE countries_seq;
 DROP SEQUENCE teams_seq;
 
+DROP SEQUENCE quarter_score_seq;
+DROP SEQUENCE overtime_score_seq;
+DROP SEQUENCE id_draft;
+DROP SEQUENCE paises;
+
+DROP SEQUENCE seq_combine_id ;
+DROP SEQUENCE seq_physical_id ;
+DROP SEQUENCE seq_athletic_id ;
+DROP SEQUENCE seq_shooting_id ;
+DROP SEQUENCE seq_pbp_player_id ;
 
 DROP TABLE TEAM_DETAILS_TEMP;
 TRUNCATE TABLE line_score_temp;
+TRUNCATE TABLE games;
+TRUNCATE TABLE game_temp;
+TRUNCATE TABLE game_info_temp;
+TRUNCATE TABLE game_summary_temp;
+
+TRUNCATE TABLE player_combine CASCADE;
+TRUNCATE TABLE Physical;
+TRUNCATE TABLE Athletic;
+TRUNCATE TABLE draft_combine_stats_temp;
+
 TRUNCATE TABLE Quarter_score;
 TRUNCATE TABLE Overtime_score;
 TRUNCATE TABLE TEAM_TEMP;
